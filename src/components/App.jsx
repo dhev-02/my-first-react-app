dfca1696f1e1b257a442b2ce9d8c67ceb4ef6282
Dhev Sabharish Sivakumar <dhevsabharish@gmail.com>
2021-08-19 15:50:03 +0530

    Styling Keeper Part 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,22 +6,16 @@ import CreateArea from "./CreateArea";
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState({});
 
-  function onType(e) {
-    var [name, value] = [e.target.name, e.target.value];
-    setNewNote({ ...newNote, [name]: value });
-  }
-
-  function onAdd(e) {
-    setNotes([...notes, newNote]);
-    setNewNote({ title: "", content: "" });
-    e.preventDefault();
-  }
-
-  function deleteItem(id) {
+  function addNote(newNote) {
     setNotes((prevNotes) => {
-      return prevNotes.filter((note, index) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
     });
@@ -30,21 +24,18 @@ function App() {
   return (
     <div>
       <Header />
-      <CreateArea
-        newNoteHeading={newNote.title}
-        newNoteContent={newNote.content}
-        onType={onType}
-        onAdd={onAdd}
-      />
-      {notes.map((note, index) => (
-        <Note
-          key={index}
-          id={index}
-          title={note.title}
-          content={note.content}
-          onDelete={deleteItem}
-        />
-      ))}
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,23 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
+import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  const [isFocused, setIsFocused] = useState(false);
+
+  function expand() {
+    setIsFocused(true);
+  }
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
-      <form>
-        <input
-          name="title"
-          placeholder="Title"
-          value={props.newNoteHeading}
-          onChange={props.onType}
-        />
+      <form className="create-note">
+        {isFocused && (
+          <input
+            name="title"
+            onChange={handleChange}
+            value={note.title}
+            placeholder="Title"
+          />
+        )}
         <textarea
           name="content"
+          onChange={handleChange}
+          value={note.content}
           placeholder="Take a note..."
-          rows="3"
-          value={props.newNoteContent}
-          onChange={props.onType}
+          rows={isFocused ? 3 : 1}
+          onFocus={expand}
         />
-        <button onClick={props.onAdd}>Add</button>
+        <Zoom in={isFocused}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
